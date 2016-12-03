8c568c61dd9301bf259c66a781cb8e4b4615aa9c
Wesley Ellis <tahnok@gmail.com>
2016-12-03 16:18:26 -0500

    fix day 1 2

--- src/bin/day1_2.rs
+++ src/bin/day1_2.rs
@@ -42,10 +42,10 @@ fn dist_to_first_revisit(instructions: &[&str]) -> Result<i32, ()> {
         };
         let distance = captures.at(2).unwrap().parse::<i32>().unwrap();
         let (new_x, new_y) = match current_direction {
-            Direction::North => move_and_check(x, y, distance, 0),
-            Direction::East => move_and_check(x, y, 0, distance),
-            Direction::South => move_and_check(x, y, -distance, 0),
-            Direction::West => move_and_check(x, y, 0, -distance),
+            Direction::North => delta_to_cords(x, y, distance, 0),
+            Direction::East => delta_to_cords(x, y, 0, distance),
+            Direction::South => delta_to_cords(x, y, -distance, 0),
+            Direction::West => delta_to_cords(x, y, 0, -distance),
         };
 
         match check_for_revisit(x, y, new_x, new_y, &mut visited) {
@@ -55,7 +55,7 @@ fn dist_to_first_revisit(instructions: &[&str]) -> Result<i32, ()> {
 
         x = new_x;
         y = new_y;
-        debug_hashset(instruction, &visited);
+        // debug_hashset(instruction, &visited);
     }
     Err(())
 }
@@ -68,28 +68,23 @@ fn debug_hashset(instruction: &str, visited: &Vec<(i32, i32)>) {
     }
 }
 
-fn move_and_check(old_x: i32, old_y: i32, delta_x: i32, delta_y: i32) -> (i32, i32) {
+fn delta_to_cords(old_x: i32, old_y: i32, delta_x: i32, delta_y: i32) -> (i32, i32) {
     let new_x = old_x + delta_x;
     let new_y = old_y + delta_y;
     (new_x, new_y)
 }
 
 fn check_for_revisit(old_x: i32, old_y: i32, new_x: i32, new_y: i32, visited: &mut Vec<(i32, i32)>) -> Result<i32, ()> {
-    println!("{}", steps(old_x, new_x));
-    for mut step in old_x..new_x {
-        step = step + 1;
+    for step in steps(old_x, new_x) {
         let pos = (step, old_y);
-        println!("x: {}, y: {}", step, old_y);
         if visited.contains(&pos) {
             return Ok(calculate_distance_tuple(pos))
         } else {
             visited.push(pos);
         }
     }
-    for mut step in old_y..new_y {
-        step = step + 1;
+    for step in steps(old_y, new_y) {
         let pos = (new_x, step);
-        println!("x: {}, y: {}", new_x, step);
         if visited.contains(&pos) {
             return Ok(calculate_distance_tuple(pos))
         } else {
@@ -101,9 +96,9 @@ fn check_for_revisit(old_x: i32, old_y: i32, new_x: i32, new_y: i32, visited: &m
 
 fn steps(a: i32, b: i32) -> Vec<i32> {
     if b > a {
-        Range{ start: a, end: b + 1}.collect()
+        Range{ start: a + 1, end: b + 1}.collect()
     } else {
-        Range{ end: a + 1, start: b }.rev().collect()
+        Range{ start: b, end: a }.rev().collect()
     }
 }
 
@@ -135,35 +130,42 @@ fn visits_all_steps_north() {
 
 #[test]
 fn visits_all_steps_south() {
-    let mut visited = vec![(0,0)];
+    let mut visited = vec![(2,0)];
     let _ = check_for_revisit(2, 0, 0, 0, &mut visited);
-    assert_eq!(vec![(0,0), (0,1), (0,2)], visited);
+    assert_eq!(vec![(2,0), (1,0), (0,0)], visited);
 }
 
 #[test]
 fn steps_works() {
-    let expected = vec![1,2,3];
+    let expected = vec![2,3];
     let result: Vec<i32> = steps(1,3);
     assert_eq!(expected, result);
 }
 
 #[test]
 fn steps_negative() {
-    let expected = vec![-8,-7,-6];
+    let expected = vec![-7,-6];
     let result: Vec<i32> = steps(-8, -6);
     assert_eq!(expected, result);
 }
 
 #[test]
 fn steps_reverse() {
-    let expected = vec![3,2,1];
+    let expected = vec![2,1];
     let result: Vec<i32> = steps(3, 1);
     assert_eq!(expected, result);
 }
 
 #[test]
 fn steps_reverse_negative() {
-    let expected = vec![-6,-7,-8];
+    let expected = vec![-7,-8];
     let result: Vec<i32> = steps(-6, -8);
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn steps_zero() {
+    let expected = vec![1,0];
+    let result: Vec<i32> = steps(2, 0);
     assert_eq!(expected, result);
 }
