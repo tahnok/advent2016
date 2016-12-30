23ccbe7f8d4b0ad7db0d878ffce824ae6b7cb59b
Wesley Ellis <tahnok@gmail.com>
2016-12-30 10:05:15 -0500

    day 17 part 1 and template

---
+++ template.rs
@@ -0,0 +1,13 @@
+use std::io;
+use std::io::Read;
+
+fn main() {
+    let mut input = String::new();
+    let _ = io::stdin().read_to_string(&mut input);
+    println!("TODO");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
